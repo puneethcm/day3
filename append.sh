14a55e6c592bc2922cd8ed396b57d5515a8ee516
puneethcm <puneethkumarcm8050@gmail.com>
2022-12-29 14:52:04 +0530

    first commit

---
+++ append.sh
@@ -0,0 +1,14 @@
+for files in `ls *.log*`
+do
+folderName=`echo $files | awk -f. '{print $1}'`;
+echo $files;
+echo $folderName;
+printf "\n";
+if [ -d $folderName ]
+then
+rm -r $folderName
+fi
+x=`date +'%Y'`;
+mkdir $folderName;
+cp $files $foldeName/$files$x;
+done
---
+++ greqater.sh
@@ -0,0 +1,8 @@
+read -p "enter the number: " a
+read -p "enter the number: " b
+if [ $a -gt $b ]
+then
+echo "$a is greater number"
+else
+echo "$b is greater number"
+fi
